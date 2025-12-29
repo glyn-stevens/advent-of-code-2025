c77dcb0a5b730250798a445690993ca51f2a2af1
glyn-stevens <glynst@gmail.com>
2025-12-29 00:03:51 +0000

    clean up day11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,18 +1,15 @@
 use aoc25::{Input, load_input};
-use std::collections::{BinaryHeap, HashMap};
-use std::hash::Hash;
-use std::ops::Add;
-use itertools::Itertools;
+use std::collections::HashMap;
 
 const DAY: u8 = 11;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Default)]
 struct RouteCombinations {
     // Number of routes to the exit node that have a 'dac' node in them, have an 'fft' node, have both, or neither.
     dac: u64,
     fft: u64,
     both: u64,
-    neither: u64
+    neither: u64,
 }
 
 impl RouteCombinations {
@@ -40,47 +37,63 @@ fn main() {
 
 fn solve_part_a(lines: &[String]) -> u64 {
     let nodes: HashMap<String, Vec<String>> = lines.iter().map(parse_node).collect();
-    let mut previously_calculated_routes = HashMap::new();
-    let start_node = "you".to_string();
-    let exit_node = "out".to_string();
-    route_combinations(&nodes, &start_node,&exit_node, &mut previously_calculated_routes).total()
+    let mut cache = HashMap::new();
+    let start_node = "you";
+    let exit_node = "out";
+    route_combinations(&nodes, &start_node, &exit_node, &mut cache).total()
 }
 
 fn solve_part_b(lines: &[String]) -> u64 {
     let nodes: HashMap<String, Vec<String>> = lines.iter().map(parse_node).collect();
-    let mut previously_calculated_routes = HashMap::new();
-    let start_node = "svr".to_string();
-    let exit_node = "out".to_string();
-    route_combinations(&nodes, &start_node,&exit_node, &mut previously_calculated_routes).both
+    let mut cache = HashMap::new();
+    let start_node = "svr";
+    let exit_node = "out";
+    route_combinations(&nodes, &start_node, &exit_node, &mut cache).both
 }
-
 
 fn route_combinations(
     all_nodes: &HashMap<String, Vec<String>>,
-    current_node: &String,
-    exit_node: &String,
-    previously_calculated_routes: &mut HashMap<String, RouteCombinations>,
+    current_node: &str,
+    exit_node: &str,
+    cache: &mut HashMap<String, RouteCombinations>,
 ) -> RouteCombinations {
-    let mut routes = RouteCombinations {dac:0, fft:0, both:0, neither:0};
-    for next_node in all_nodes[current_node].iter() {
+    let mut routes = RouteCombinations::default();
+    for next_node in all_nodes
+        .get(current_node)
+        .expect("Couldn't find current node")
+        .iter()
+    {
         if next_node == exit_node {
             routes.neither += 1;
-        } else if let Some(solutions_from_next_node) = previously_calculated_routes.get(next_node) {
-            routes = routes.combine(&update_routes_with_node(&current_node, *solutions_from_next_node));
+        } else if let Some(solutions_from_next_node) = cache.get(next_node.as_str()) {
+            routes = routes.combine(&update_routes_with_node(
+                current_node,
+                *solutions_from_next_node,
+            ));
         } else {
-            let routes_from = route_combinations(all_nodes, next_node, exit_node, previously_calculated_routes);
+            let routes_from = route_combinations(all_nodes, next_node, exit_node, cache);
             routes = routes.combine(&update_routes_with_node(&current_node, routes_from));
         }
     }
-    previously_calculated_routes.insert(current_node.clone(), routes.clone());
+    cache.insert(current_node.to_string(), routes);
     routes
 }
 
-fn update_routes_with_node(node_to_add: &String, routes: RouteCombinations) -> RouteCombinations {
-    match node_to_add.as_str() {
-        "dac" => RouteCombinations {dac: routes.neither + routes.dac, fft:0, both: routes.fft + routes.both, neither:0},
-        "fft" => RouteCombinations {dac:0, fft: routes.neither + routes.fft, both: routes.dac + routes.both, neither:0},
-        _ => routes
+fn update_routes_with_node(node_to_add: &str, routes: RouteCombinations) -> RouteCombinations {
+    match node_to_add {
+        "dac" => RouteCombinations {
+            dac: routes.neither + routes.dac,
+            fft: 0,
+            both: routes.fft + routes.both,
+            neither: 0,
+        },
+        "fft" => RouteCombinations {
+            dac: 0,
+            fft: routes.neither + routes.fft,
+            both: routes.dac + routes.both,
+            neither: 0,
+        },
+        _ => routes,
     }
 }
 
@@ -105,6 +118,9 @@ mod tests {
     #[test]
     fn test_part_b() {
         assert_eq!(2, solve_part_b(&load_input(DAY, Input::TestB)));
-        assert_eq!(533996779677200, solve_part_b(&load_input(DAY, Input::Puzzle)));
+        assert_eq!(
+            533996779677200,
+            solve_part_b(&load_input(DAY, Input::Puzzle))
+        );
     }
 }
